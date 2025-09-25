ab14cf068623831de885f4d12d7d290f04e871bb
jshen318 <jshen@olin.edu>
2025-09-25 13:26:09 -0400

    Comments & changes for readability

--- python/PythonReceiveData.py
+++ python/PythonReceiveData.py
@@ -22,54 +22,39 @@ import serial
 #
 
 
-#
 # Set the name of the serial port.  Determine the name as follows:
-#	1) From Arduino's "Tools" menu, select "Port"
-#	2) It will show you which Port is used to connect to the Arduino
+# 	1) From Arduino's "Tools" menu, select "Port"
+# 	2) It will show you which Port is used to connect to the Arduino
 #
 # For Windows computers, the name is formatted like: "COM6"
 # For Apple computers, the name is formatted like: "/dev/tty.usbmodemfa141"
-#
 arduinoComPort = "/dev/ttyACM0"
 
 
-#
 # Set the baud rate
 # NOTE1: The baudRate for the sending and receiving programs must be the same!
 # NOTE2: For faster communication, set the baudRate to 115200 below
 #        and check that the arduino sketch you are using is updated as well.
-#
 baudRate = 9600
 
-
-#
 # open the serial port
-#
 serialPort = serial.Serial(arduinoComPort, baudRate, timeout=1)
 
 lines = []
 
-#
 # main loop to read data from the Arduino, then display it
-#
 while True:
-  #
-  # ask for a line of data from the serial port, the ".decode()" converts the
-  # data from an "array of bytes", to a string
-  #
-  lineOfData = serialPort.readline().decode()
+    # ask for a line of data from the serial port, the ".decode()" converts the
+    # data from an "array of bytes", to a string
+    lineOfData = serialPort.readline().decode()
 
-  #
-  # check if data was received
-  #
-  if len(lineOfData) > 0:
-    #
-    # data was received, convert it into 4 integers
-    #
-    print(lineOfData)
-    lines.append(lineOfData)
-    if "DONE" in lineOfData:
-        print ("wrotefile")
-        with open("./data.txt", "w", encoding="utf-8") as f:
-            f.write('\n'.join(str(i) for i in lines));
-    continue
+    # check if data was received
+    if len(lineOfData) > 0:
+        # data was received, convert it into 4 integers
+        print(lineOfData)
+        lines.append(lineOfData)
+        if "DONE" in lineOfData:
+            print("wrotefile")
+            with open("./data.txt", "w", encoding="utf-8") as f:
+                f.write("\n".join(str(i) for i in lines))
+        continue
--- python/calibration.py
+++ python/calibration.py
@@ -2,17 +2,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.polynomial import Polynomial
 
+# Load calibration measurements.
 calibration = np.loadtxt("./Calibration_data.csv", delimiter=",", dtype=int)
 
+# Create a line of best fit for the calibration data.
+# The first column is centimeters, the second is recorded analog output.
 cm = calibration[:, 0]
 v = calibration[:, 1]
 equation = Polynomial.fit(v, cm, 1)
+
+
+# Function to return the conversion equation.
 def get_calibration_equation():
     return equation
 
+
+# Function to plot the calibration equation.
 def plot_calibration_equation():
     ax = plt.figure().add_subplot()
     t = np.linspace(350, 600, 100)
-    ax.scatter(v, cm)
-    ax.plot(t, equation(t))
+    ax.scatter(v, cm, label="Measured Data")
+    ax.plot(t, equation(t), label="Calibration Line")
+    ax.set_xlabel("Analog Voltage Output")
+    ax.set_ylabel("Distance (cm)")
+    ax.set_title("Calibration Curve")
+    ax.legend()
     plt.show()
+
+
+plot_calibration_equation()
+
+
+# Function to plot the equation that the rubric asks for.
+def plot_reverse_calibration():
+    equation_2 = Polynomial.fit(cm, v, 1)
+    ax = plt.figure().add_subplot()
+    t = np.linspace(10, 35, 100)
+    ax.scatter(cm, v, label="Measured Data")
+    ax.plot(t, equation_2(t), label="Calibration Line")
+    ax.set_ylabel("Analog Voltage Output")
+    ax.set_xlabel("Distance (cm)")
+    ax.set_title("Reversed Calibration Curve")
+    ax.legend()
+    plt.show()
+
+
+plot_reverse_calibration()
