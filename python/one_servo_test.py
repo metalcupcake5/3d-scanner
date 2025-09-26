ed076216f3931de28aa2cc049d0703ead36771ac
metalcupcake5 <43897385+metalcupcake5@users.noreply.github.com>
2025-09-25 21:16:12 -0400

    change error plotting to new data

--- python/one_servo_test.py
+++ python/one_servo_test.py
@@ -2,30 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 from calibration import get_calibration_equation
 
-with open("data1_format.txt") as f:
+with open("data_new_format.txt") as f:
     lines = f.readlines()
 
 values = [int(x) for x in lines]
-values = np.reshape(values, [17, 81])
+values = np.reshape(values, [66, 66])
 values = get_calibration_equation()(values)
-values = values[8, ...]
+values = values[33, ...]
 print(values)
 
 # horizontal angle
-theta = np.divide(range(50, 131, 1), 180) * np.pi
+theta = np.divide(range(60, 125, 1), 180) * np.pi
 
 x = []
 y = []
 
 for t in range(len(theta)):
     dist = values[t]
-    if dist > 30:
+    if dist > 35:
         continue
     x.append(dist * np.cos(theta[t]))
     y.append(dist * np.sin(theta[t]))
 
 # Create an array to plot of the correct distance.
-correct_dist = np.ones(len(x)) * 26
+correct_dist = np.ones(len(x)) * 30
 # Plot the sensor output against the correct values.
 plt.plot(x, y, "o", label="Sensor Output")
 plt.title("2D Scatterplot of 1-Servo Scan")
@@ -40,10 +40,10 @@ plt.show()
 # Calculate the error.
 error = np.divide(y - correct_dist, correct_dist)
 # Plot the error in a scatterplot.
-plt.plot(x, error, "o")
+plt.plot(x, error * 100, "o")
 plt.plot(x, np.zeros(len(x)), label="0% Error")
 plt.title("1-Servo Error Scatterplot")
 plt.xlabel("Horizontal Distance (cm)")
-plt.ylabel("Error")
+plt.ylabel("Error (%)")
 plt.legend()
 plt.show()
